21d408741818f0a83b5d951af7ae7f8f85387609
Julien Le Mee <julemee@gmail.com>
2023-04-11 16:56:03 -0400

    continue slider

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const slider = tns({
-  container: '.slider__wrapper',
-  items: 1,
-  slideBy: 'page',
-  autoplay: true,
-  controls: false,
-  navPosition: 'bottom',
-  mouseDrag: true,
-  swipeAngle: false,
-  speed: 400
-});
+import {tns} from './src/tiny-slider.js';
+
+  var slider = tns({
+    container: '.my-slider',
+    items: 3,
+    slideBy: 'page',
+    autoplay: true
+  });
+
+// const slider = tns({
+//   container: '.slider__wrapper',
+//   items: 1,
+//   slideBy: 'page',
+//   autoplay: true,
+//   controls: false,
+//   navPosition: 'bottom',
+//   mouseDrag: true,
+//   swipeAngle: false,
+//   speed: 400
+// });
 
 document.querySelector('.slider__prev').addEventListener('click', () => {
   slider.goTo('prev');
