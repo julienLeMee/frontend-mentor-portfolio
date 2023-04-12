09fdd0bc6bc420474576fd623e8a41308c97287d
Julien Le Mee <julemee@gmail.com>
2023-04-12 08:32:30 -0400

    added slider

--- script.js
+++ script.js
@@ -1,28 +1,20 @@
-import {tns} from './src/tiny-slider.js';
-
-  var slider = tns({
-    container: '.my-slider',
-    items: 3,
-    slideBy: 'page',
-    autoplay: true
-  });
-
-// const slider = tns({
-//   container: '.slider__wrapper',
-//   items: 1,
-//   slideBy: 'page',
-//   autoplay: true,
-//   controls: false,
-//   navPosition: 'bottom',
-//   mouseDrag: true,
-//   swipeAngle: false,
-//   speed: 400
-// });
-
-document.querySelector('.slider__prev').addEventListener('click', () => {
-  slider.goTo('prev');
-});
-
-document.querySelector('.slider__next').addEventListener('click', () => {
-  slider.goTo('next');
+let slider = tns({
+  container: '.my-slider',
+  items: 3,
+  slideBy: '1',
+  controlsPosition: 'bottom',
+  nav: false,
+  prevButton: '.slider__prev',
+  nextButton: '.slider__next',
+  responsive: {
+    320: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1024: {
+      items: 3,
+    },
+  },
 });
